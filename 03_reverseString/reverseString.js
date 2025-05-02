18057a98af6e8d74bf2a4f1557dd19cf555e9b20
Ezra1859 <ezra.tsegaye@outlook.com>
2025-05-02 22:46:22 +0100

    Complete the first 6 js tasks

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverse = '';
+    let i = word.length - 1;
+    while (i >= 0) {
+        reverse += word[i];
+        i--;
+    }
+    return reverse;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if ((a | b) < 0) return "ERROR";
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    let sum = 0;
+    if (a > b) [a, b] = [b, a];
+    let i = a;
+    while (i <= b) {
+        sum += i;
+        i++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
